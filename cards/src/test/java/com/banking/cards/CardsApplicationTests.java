132cec8d19df019ffecc3503b42465e5f1f247ad
sen-harshit03 <harshitsen03@gmail.com>
2023-10-19 04:07:35 +0530

    Added eureka server, changed mysql to h2 only for acc, loans, cards not for dockercompose (2)

--- cards/src/test/java/com/banking/cards/CardsApplicationTests.java
+++ cards/src/test/java/com/banking/cards/CardsApplicationTests.java
@@ -1,13 +1,16 @@
 package com.banking.cards;
 
+import net.minidev.json.writer.BeansMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class CardsApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
